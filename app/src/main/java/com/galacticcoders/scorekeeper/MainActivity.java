e6bd588481bcea539299207ff90b46d2fa641a50
dragosh craciun <dragosh.craciun@gmail.com>
2018-01-14 11:58:24 +0100

    fix: Stop score reset on screen orientation change
    When the orientation of the screen changes the the current
    activity gets destroyed and recreated. The solution was
    to implement onSaveInstanceState() so that before the activity
    gets destroyed the variables are saved and upon the recreation
    of the activity the variables will be restored.

--- app/src/main/java/com/galacticcoders/scorekeeper/MainActivity.java
+++ app/src/main/java/com/galacticcoders/scorekeeper/MainActivity.java
@@ -7,15 +7,33 @@ import android.widget.TextView;
 
 import com.galacticcoders.scorekeeper.R;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     float akaScore = 0;
     float shiroScore = 0;
 
+    private static final String AKA_SCORE_KEY = "aka_score_key";
+    private static final String SHIRO_SCORE_KEY = "shiro_score_key";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            akaScore = savedInstanceState.getFloat(AKA_SCORE_KEY);
+            shiroScore = savedInstanceState.getFloat(SHIRO_SCORE_KEY);
+            displayForAka((int) akaScore);
+            displayForShiro((int) shiroScore);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putFloat(AKA_SCORE_KEY, akaScore);
+        outState.putFloat(SHIRO_SCORE_KEY, shiroScore);
     }
 
     public void resetScore(View v) {
